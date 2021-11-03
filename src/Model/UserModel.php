712de854fa76be185f271bc22b05bdf4f94c1afe
bastienwcs <bastien.krettly@wildcodeschool.com>
2021-11-03 11:51:56 +0100

    fixes

--- src/Model/UserModel.php
+++ src/Model/UserModel.php
@@ -11,7 +11,7 @@ class UserModel
         'phone' => '(911)-579-9788'
     ];
 
-    // TODO: create a sign in fonction
+    // TODO: create a sign in fonction and store the user infos in session
 
-    // TODO: create a sign out fonction
+    // TODO: create a sign out fonction and clear the session
 }
--- src/View/_navigation.php
+++ src/View/_navigation.php
@@ -10,6 +10,6 @@
             <a href="/logout">Sign out</a>
         </li>
     </ul>
-    <!-- Bonus: show/hide the Sign in, Sign Out and Profile links when necessary -->
-    <!-- Bonus: show the name and picture of the logged in user -->
+    <!-- BONUS: show/hide the Sign in, Sign Out and Profile links when necessary -->
+    <!-- BONUS: show the name and picture of the logged in user -->
 </nav>
