6d62bc0f80ebfca61f6c3b88e774b11a19227cef
bastienwcs <bastien.krettly@wildcodeschool.com>
2021-11-03 11:34:22 +0100

    fix bonuses

--- src/View/_navigation.php
+++ src/View/_navigation.php
@@ -4,11 +4,12 @@
             <a href="/login">Sign in</a>
         </li>
         <li>
-            <a href="/logout">Sign out</a>
-        </li>
-        <li>
             <a href="/profile">Profile</a>
         </li>
+        <li>
+            <a href="/logout">Sign out</a>
+        </li>
     </ul>
-    <!-- Bonus: show the name and picture of the user if logged in -->
+    <!-- Bonus: show/hide the Sign in, Sign Out and Profile links when necessary -->
+    <!-- Bonus: show the name and picture of the logged in user -->
 </nav>
